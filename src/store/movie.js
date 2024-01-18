8fe96e8a673c24c338e91d55897de1a0357875ba
jmpark24 <jmpark@gmail.com>
2024-01-18 22:36:12 +0900

    Add Serverless Functions

--- src/store/movie.js
+++ src/store/movie.js
@@ -19,7 +19,13 @@ export const searchMovies = async page => {
     store.state.message = '';
   }
   try {
-    const res = await fetch(`https://www.omdbapi.com/?apikey=96b42200&s=${store.state.searchText}&page=${page}`);
+    const res = await fetch('/api/movie', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: store.state.searchText,
+        page
+      })
+    });
     const { Search, totalResults, Response, Error }= await res.json();
     if (Response === 'True') {
       store.state.movies = [
@@ -39,7 +45,12 @@ export const searchMovies = async page => {
 }
 export const getMovieDetails = async id => {
   try {
-    const res = await fetch(`https://www.omdbapi.com/?apikey=96b42200&i=${id}&plot=full`);
+    const res = await fetch('./api/movie', {
+      method: 'POST',
+      body: JSON.stringify({
+        id
+      })
+    })
     store.state.movie = await res.json();
   } catch (error) {
     console.log('getMovieDetails error:', error);
